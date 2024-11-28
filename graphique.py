951147951ef368e9e79f7b7080f07a4b3b7fe619
AlexisPOLLET <alexis.pollet@etu.unilasalle.fr>
2024-11-28 12:30:34 +0100

    Update graphique.py

--- graphique.py
+++ graphique.py
@@ -5,11 +5,11 @@ import plotly.express as px
 # Titre de l'application
 st.title("Analyse des Séismes en France entre 1990 et 2023")
 
-# Étape 1 : Téléchargement du fichier CSV
+# Étape 1.1 : Téléchargement du fichier CSV
 uploaded_file = st.file_uploader("Téléchargez un fichier CSV contenant les données des séismes", type=["csv"])
 
 if uploaded_file:
-    # Étape 2 : Lecture du fichier CSV
+    # Étape 1.2 : Lecture du fichier CSV
     try:
         data = pd.read_csv(uploaded_file)
         st.success("Fichier chargé avec succès !")
@@ -25,7 +25,7 @@ if uploaded_file:
             st.subheader("Statistiques descriptives des données :")
             st.write(data.describe())
             
-            # Étape 3 : Filtrer les données pour la France
+            # Étape 1.3 : Filtrer les données pour la France
             st.subheader("Statistiques des séismes en France")
             
         # Vérifiez que les colonnes nécessaires existent
@@ -40,7 +40,7 @@ if uploaded_file:
             show_graph = st.checkbox("Afficher le graphique des séismes en France par année depuis 1990")
             
             if show_graph:
-                # Étape 4 : Diagramme en barres avec Plotly
+                # Étape 1.4 : Diagramme en barres avec Plotly
                 st.subheader("Frise chronologique des séismes")
                 fig = px.bar(
                     seismes_par_annee,
@@ -53,9 +53,9 @@ if uploaded_file:
                 st.plotly_chart(fig)
 
 
-        # Vérification des colonnes nécessaires
+        #  Etape 2.1 : Vérification des colonnes nécessaires
         if 'latitude' in data.columns and 'longitude' in data.columns and 'significance' in data.columns:
-            # Étape 2 : Filtrer les séismes par niveau de significance
+            # Étape 2.1 : Filtrer les séismes par niveau de significance
             st.subheader("Analyse des Séismes par Niveau de Significance")
 
             low_significance = data[data['significance'] < 50]
@@ -66,7 +66,7 @@ if uploaded_file:
             st.write(f"Nombre de séismes de moyenne significance : {len(medium_significance)}")
             st.write(f"Nombre de séismes de forte significance : {len(high_significance)}")
 
-            # Étape 3 : Créer une carte interactive avec Plotly
+            # Étape 2.2 : Créer une carte interactive avec Plotly
             st.subheader("Carte Interactive des Séismes")
             fig_map = px.scatter_mapbox(
                 data,
@@ -76,7 +76,7 @@ if uploaded_file:
                 size="significance",  # Taille des points selon la significance
                 hover_name="significance",
                 title="Carte des Séismes en France",
-                zoom=5,
+                zoom=2,
                 mapbox_style="carto-positron"
             )
             st.plotly_chart(fig_map)
